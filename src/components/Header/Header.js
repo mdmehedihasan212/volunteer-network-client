35b04ffe274e5a61c47e10187a1a07919a2a7f16
mdmehedihasan212 <mdmehedihasan384@gmail.com>
2022-04-26 15:12:00 +0600

    setup router firebase and header

---
+++ src/components/Header/Header.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import logo from '../../logos/Group 1329.png';
+
+const Header = () => {
+    return (
+        <section>
+            <div>
+                <img src={logo} alt="" />
+            </div>
+        </section>
+    );
+};
+
+export default Header;
